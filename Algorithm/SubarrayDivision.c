aa09c07ff7b57b1c36b825782414912445784cf4
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-01-07 23:19:22 +0530

    Create SubarrayDivision.c

---
+++ Algorithm/SubarrayDivision.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int target,blocks;
+    scanf("%d",&target);
+    scanf("%d",&blocks);
+}
