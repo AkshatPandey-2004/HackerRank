ed853fa67ebc7105310e2287ecdb286d98d5dcfb
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-01-16 23:15:50 +0530

    Create loop1.java

---
+++ Java/loop1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+
+
+public class Solution {
+    public static void main(String[] args){
+        int N,i=1;
+        Scanner sc = new Scanner(System.in);
+        N=sc.nextInt();
+        for(i=1;i<=10;i++){
+            System.out.println(N+" x "+ i +" = "+N*i);
+        }
+
+    }
+}
