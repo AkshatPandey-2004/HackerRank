9a03979a107f6e66699be56a55db1749b7e4e1e4
Akshat Pandey <akshatpandey26.2004@gmail.com>
2024-01-31 21:04:42 +0530

    Create Day7.java

---
+++ 30DaysofCode/Java/Day7.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        int i=n-1;
+        while(i>=0){
+            System.out.printf("%d ",array[i]);
+            i--;
+        }
+    }
+}
